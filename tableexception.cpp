93f38243ff48afbb558184915c4ab45d0eb00cfa
Sergey Doroshin <sergeybezzabotnovv@gmail.com>
2024-05-06 16:48:27 +0300

    Add string to exception

--- tableexception.cpp
+++ tableexception.cpp
@@ -1,6 +1,10 @@
 #include "tableexception.h"
 
 TableException::TableException(const char* messg)
-     : std::exception(messg) {}
+    : std::exception(), m_message{ messg } {}
+const char* TableException::what() const
+{
+    return m_message.c_str();
+}
 {
 }
--- tableexception.h
+++ tableexception.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <exception>
+#include <string>
 
 class TableException : public std::exception {
 public:
     TableException(const char* messg);
+
+private:
+    std::string m_message;
 };
